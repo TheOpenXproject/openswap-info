0b4b5f50ed8ac0c858b8ac24da048ae8f81867a0
Lucio <lesolari_13@hotmail.com>
2021-11-10 20:40:51 -0300

    Fixed etherscan

--- src/components/Warning/index.js
+++ src/components/Warning/index.js
@@ -28,14 +28,14 @@ export default function Warning({ type, show, setShow, address }) {
         tokens that claim to represent projects that do not have a token.
       </Text>
       <Text fontWeight={500} lineHeight={'145.23%'} mt={'10px'}>
-        Similar to Etherscan, this site automatically tracks analytics for all ERC20 tokens independent of token
+        Similar to the Explorer, this site automatically tracks analytics for all ERC20 tokens independent of token
         integrity. Please do your own research before interacting with any ERC20 token.
       </Text>
     </div>
   ) : (
     <Text fontWeight={500} lineHeight={'145.23%'} mt={'10px'}>
       Anyone can create and name any ERC20 token on Harmony, including creating fake versions of existing tokens and
-      tokens that claim to represent projects that do not have a token. Similar to Etherscan, this site automatically
+      tokens that claim to represent projects that do not have a token. Similar to the Explorer, this site automatically
       tracks analytics for all ERC20 tokens independent of token integrity. Please do your own research before
       interacting with any ERC20 token.
     </Text>
